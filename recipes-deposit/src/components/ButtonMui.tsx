6056d90dc461a3cd19cbd743eccc5cbfc8f70f76
vfelgr <vaclav.felgr@cleverlance.com>
2024-04-02 15:59:10 +0200

    commit 0010: fixed root folder,

--- recipes-deposit/src/components/ButtonMui.tsx
+++
@@ -1,9 +0,0 @@
-import { Button, type ButtonProps } from '@mui/material';
-
-export const ButtonMui: React.FC = ({ color }: ButtonProps) => {
-    return (
-        <div>
-            <Button color={color}>Add recipe</Button>
-        </div>
-    );
-};
